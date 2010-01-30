900fd15866eda56828695df1cebb77368975e381
blatyo <blatyo@gmail.com>
2010-01-30 11:48:06 -0500

    Changed the name of some tests to be autotest friendly.

---
+++ spec/bencode/integer_spec.rb
@@ -0,0 +1,72 @@
+# encoding: UTF-8
+
+require "spec"
+require "spec_helper"
+
+describe Integer do
+  describe "#bencode" do
+    it "should encode a positive integer" do
+      1.bencode.should == "i1e"
+    end
+
+    it "should encode a negative integer" do
+      -1.bencode.should == "i-1e"
+    end
+
+    it "should encode a positive big integer" do
+      10_000_000_000.bencode.should == "i10000000000e"
+    end
+
+    it "should encode a negative big integer" do
+      -10_000_000_000.bencode.should == "i-10000000000e"
+    end
+  end
+end
+
+describe Numeric do
+  describe "#bencode" do
+    it "should encode a positive float with precision loss" do
+      1.1.bencode.should == "i1e"
+    end
+
+    it "should encode a negative float with precision loss" do
+      -1.1.bencode.should == "i-1e"
+    end
+
+    it "should encode an positive exponential float" do
+      1e10.bencode.should == "i10000000000e"
+    end
+
+    it "should encode an negative exponential float" do
+      -1e10.bencode.should == "i-10000000000e"
+    end
+  end
+end
+
+describe Time do
+  describe "#bencode" do
+    it "should encode to bencoding" do
+      Time.at(4).bencode.should == "i4e"
+    end
+  end
+end
+
+describe BEncode::Integer do
+  describe "#register" do
+    context "once an object has been registered as a BEncode integer" do
+      before :all do
+        BEncode::Integer.register NilClass
+      end
+
+      context "an instance of that object" do
+        it "should respond to bencode" do
+          nil.should respond_to :bencode
+        end
+
+        it "should encode to a bencoded integer" do
+          nil.bencode.should == "i0e"
+        end
+      end
+    end
+  end
+end
---
+++ spec/bencode/list_spec.rb
@@ -0,0 +1,36 @@
+# encoding: UTF-8
+
+require "spec"
+require "spec_helper"
+
+describe Array do
+  describe "#bencode" do
+    it "should encode an empty array" do
+      [].bencode.should == "le"
+    end
+
+    it "should encode an array filled with bencodable objects" do
+      [:e, "a", 1, Time.at(11)].bencode.should == "l1:e1:ai1ei11ee"
+    end
+  end
+end
+
+describe BEncode::List do
+  describe "#register" do
+    context "once an object has been registered as a BEncode list" do
+      before :all do
+        BEncode::List.register Range
+      end
+
+      context "an instance of that object" do
+        it "should respond to bencode" do
+          (1..2).should respond_to :bencode
+        end
+
+        it "should encode to a bencoded list" do
+          (1..2).bencode.should == "li1ei2ee"
+        end
+      end
+    end
+  end
+end
