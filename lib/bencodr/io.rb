d00e6720e4a96c9cc0301d09c30d2f0dafd3087d
quaternion <pkskynet@tut.by>
2012-03-23 16:01:44 +0400

    fix "invalid byte sequence in UTF-8" error when decoding files
    add necessary tests
    remove force_encoding from File#bdecode

--- lib/bencodr/io.rb
+++ lib/bencodr/io.rb
@@ -6,20 +6,20 @@ module BEncodr
       def bencode(fd, object)
         open(fd, "wb") {|file| file.bencode(object)}
       end
-      
+
       def bdecode(fd)
         open(fd, "rb") {|file| file.bdecode}
       end
     end
-    
+
     def bencode(object)
       write(Object.bencode(object))
     end
-    
+
     def bdecode
-      Object.bdecode(read.force_encoding('UTF-8'))
+      Object.bdecode(read)
     end
-    
+
     def self.included(base)
       base.extend ClassMethods
     end
