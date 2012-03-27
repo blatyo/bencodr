969815b72514e00d3fccbd685510f8a5ee52a977
Allen Madsen <blatyo@gmail.com>
2012-03-26 20:44:25 -0400

    Attempt to fix specs on travis for 1.8.7

--- lib/bencodr/parser.rb
+++ lib/bencodr/parser.rb
@@ -38,7 +38,11 @@ module BEncodr
         length = scanner.scan(/[1-9][0-9]*|0/)        or raise BEncodeError, "Invalid string: length invalid. #{scanner.pos}"
         scanner.scan(/:/)                             or raise BEncodeError, "Invalid string: missing colon(:). #{scanner.pos}"
         byte_string = scanner.scan(/.{#{length}}/m)   or raise BEncodeError, "Invalid string: length too long(#{length}) #{scanner.pos}."
-        byte_string.encode('UTF-8') rescue byte_string.force_encoding('UTF-8')
+        if RUBY_VERSION =~ /1\.9/
+          byte_string.encode('UTF-8') rescue byte_string.force_encoding('UTF-8')
+        else
+          byte_string
+        end
       end
 
       # This method parases a bencoded integer.
